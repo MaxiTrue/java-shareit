17eefc42eebf550426234f243ed4a424a478cc2f
YarovicynMI <yarovitsyn96@mail.ru>
2022-07-10 23:53:11 +0400

    fix: Изменил имя класса BookingController на BookController, ругался что конфликтуют бины с одинаковыми названиями, при реализации логики класса выясню почему

--- src/main/java/ru/practicum/shareit/booking/controller/BookController.java
+++ src/main/java/ru/practicum/shareit/booking/controller/BookController.java
@@ -3,10 +3,8 @@ package ru.practicum.shareit.booking.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * // TODO .
- */
+
 @RestController
 @RequestMapping(path = "/bookings")
-public class BookingController {
+public class BookController {
 }
